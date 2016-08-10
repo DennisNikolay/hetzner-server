874433faf3907a14c333e0499e9a2bd4cc384bc9
dennis nikolay <dennis.nikolay@gmx.de>
2016-08-10 16:50:04 +0200

    Added cs-1.6 docker and finilized teamspeak docker

---
+++ docker/cs1.6.docker/run_csserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "y\n" | /home/csserver/csserver.sh debug
--- docker/teamspeak.docker/start_teamspeak.sh
+++ docker/teamspeak.docker/start_teamspeak.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-su teamspeak
-/usr/bin/teamspeak3-server_linux_amd64/ts3server_startscript.sh start inifile=/home/teamspeak/ts3server.ini
+/usr/bin/teamspeak3-server_linux_amd64/ts3server_startscript.sh start inifile=/home/teamspeak/config/ts3server.ini
 sleep 10
-tail /home/teamspeak/server/logs/ts3server_0.log
+tail -f /home/teamspeak/logs/ts3server_1.log
